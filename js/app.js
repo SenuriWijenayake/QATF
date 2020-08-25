b0b49028a8f80e644ae5fa4201b7784bf8758765
Senuri Wijenayake <senuri.wijenayake@gmail.com>
2020-08-25 19:24:06 +1000

    changed deadlines

--- js/app.js
+++ js/app.js
@@ -848,7 +848,7 @@ app.controller('HomeController', function($scope, $http, $window, $timeout) {
   };
 
   //Timer to complete answers
-  var countDownDate = new Date("Aug 24, 2020 00:00:00").getTime();
+  var countDownDate = new Date("Aug 27, 2020 17:00:00").getTime();
 
   // Update the count down every 1 second
   var x = setInterval(function() {
